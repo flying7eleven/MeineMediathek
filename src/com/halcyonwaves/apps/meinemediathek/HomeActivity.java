228dfb73129af6132aad8a207ea1811f067cfe3b
Tim Hütz <tim@huetz.biz>
2012-11-15 16:38:36 -0500

    Added the initial project definition to start an empty app

---
+++ src/com/halcyonwaves/apps/meinemediathek/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.halcyonwaves.apps.meinemediathek;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+
+	@Override
+	protected void onCreate( Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+		this.setContentView( R.layout.activity_home );
+	}
+}
