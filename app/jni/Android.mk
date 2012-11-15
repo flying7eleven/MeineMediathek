0acb3e20ff95d46ac81b1dedf7cce16c5605d653
Tim Hütz <tim@huetz.biz>
2012-11-15 18:50:30 -0500

    Added the native code required for supporting the MMS protocol

--- app/jni/Android.mk
+++ app/jni/Android.mk
@@ -1,22 +1,29 @@
-# Copyright (C) 2009 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 LOCAL_PATH := $(call my-dir)
 
+#
+# Compile libiconv
+# 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libiconv
+LOCAL_SRC_FILES := libiconv/libiconv.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+#
+# Compile libmms
+#
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := hello-jni
-LOCAL_SRC_FILES := hello-jni.c
+LOGLEVELS := ERROR WARN INFO DEBUG VERBOSE
+cflags_loglevels := $(foreach ll,$(LOGLEVELS),-DAACD_LOGLEVEL_$(ll))
 
+LOCAL_MODULE    := libmms_app
+LOCAL_SRC_FILES := libmms-0.6.2/mms.c \
+					libmms-0.6.2/mmsh.c \
+					libmms-0.6.2/mmsx.c \
+					libmms-0.6.2/uri.c \
+					libmms-0.6.2/myio.c 
+LOCAL_CFLAGS	:= -DHAVE_CONFIG_H $(cflags_loglevels)
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/libmms-0.6.2 $(LOCAL_PATH)/libiconv/include
+LOCAL_SHARED_LIBRARIES := libiconv
+LOCAL_LDLIBS 	:= -llog -lm
 include $(BUILD_SHARED_LIBRARY)
