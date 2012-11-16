a9f0025da369ca3ec7018528b8a3edc34a40b1b8
Tim Hütz <tim@huetz.biz>
2012-11-15 21:42:47 -0500

    Added a better (but basic) download UI and doing the download in background

--- app/src/com/halcyonwaves/apps/meinemediathek/fragments/MovieDownloadFragment.java
+++ app/src/com/halcyonwaves/apps/meinemediathek/fragments/MovieDownloadFragment.java
@@ -1,58 +1,38 @@
 package com.halcyonwaves.apps.meinemediathek.fragments;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 import android.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.halcyonwaves.apps.meinemediathek.R;
-import com.halcyonwaves.apps.meinemediathek.ndk.MMSInputStream;
+import com.halcyonwaves.apps.meinemediathek.worker.DownloadStream;
 
 public class MovieDownloadFragment extends Fragment {
 
 	private Button btn = null;
+	private ProgressBar pb = null;
+	private TextView tv = null;
 
-	private File file = null;
-	private FileOutputStream out = null;
-	
-    private void createOutput() throws IOException {
-        file = File.createTempFile( "streamer-downloading", ".wmv");
-        out = new FileOutputStream( file );   
-    }
-	
 	@Override
 	public View onCreateView( final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState ) {
-		View v = inflater.inflate( R.layout.fragment_moviedownload, container );
+		final View v = inflater.inflate( R.layout.fragment_moviedownload, container );
 		this.btn = (Button) v.findViewById( R.id.btn_download );
+		this.pb = (ProgressBar) v.findViewById( R.id.pb_download_progress_pct );
+		this.tv = (TextView) v.findViewById( R.id.tv_download_progress_bytes );
+
 		this.btn.setOnClickListener( new OnClickListener() {
 
 			@Override
-			public void onClick( View v ) {
-				try {
-					MMSInputStream st = new MMSInputStream( "mms://a1014.v1252931.c125293.g.vm.akamaistream.net/7/1014/125293/v0001/wm.od.origin.zdf.de.gl-systemhaus.de/none/zdf/12/08/120827_trailer_staffel13_kio_sok4_vh.wmv" );
-					MovieDownloadFragment.this.createOutput();
-					byte[] buffer = new byte[ 8192 ];
-					int comReadB  = 0;
-					while( comReadB < st.length() ) {
-						int readB = st.read( buffer, 0, buffer.length );
-						if( readB <= 0 ) {
-							break;
-						}
-						out.write( buffer, 0, readB );
-						comReadB += readB;
-					}
-					out.close();
-				} catch( IOException e ) {
-					Log.e( "SearchMoveFragment", e.getMessage() );
-				}
+			public void onClick( final View v ) {
+
+				final DownloadStream dst = new DownloadStream( MovieDownloadFragment.this.pb, MovieDownloadFragment.this.tv );
+				dst.execute( "mms://a1014.v1252931.c125293.g.vm.akamaistream.net/7/1014/125293/v0001/wm.od.origin.zdf.de.gl-systemhaus.de/none/zdf/12/08/120827_trailer_staffel13_kio_sok4_vh.wmv" );
 
 			}
 		} );
--- app/src/com/halcyonwaves/apps/meinemediathek/ndk/MMSInputStream.java
+++ app/src/com/halcyonwaves/apps/meinemediathek/ndk/MMSInputStream.java
@@ -1,27 +1,12 @@
 /*
-** AACPlayer - Freeware Advanced Audio (AAC) Player for Android
-** Copyright (C) 2011 Spolecne s.r.o., http://www.spoledge.com
-**  
-** This program is free software; you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation; either version 3 of the License, or
-** (at your option) any later version.
-** 
-** This program is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-** GNU General Public License for more details.
-** 
-** You should have received a copy of the GNU General Public License
-** along with this program. If not, see <http://www.gnu.org/licenses/>.
-**/
+ * * AACPlayer - Freeware Advanced Audio (AAC) Player for Android* Copyright (C) 2011 Spolecne s.r.o., http://www.spoledge.com** This program is free software; you can redistribute it and/or modify* it under the terms of the GNU General Public License as published by* the Free Software Foundation; either version 3 of the License, or* (at your option) any later version.** This program is distributed in the hope that it will be useful,* but WITHOUT ANY WARRANTY; without even the implied warranty of* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the* GNU General Public License for more details.** You should have received a copy of the GNU General Public License* along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.halcyonwaves.apps.meinemediathek.ndk;
 
-import android.util.Log;
-
-import java.io.InputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
+import android.util.Log;
 
 /**
  * This is a MMS input stream - reads data from a MMS stream.
@@ -35,127 +20,123 @@ import java.io.IOException;
  */
 public class MMSInputStream extends InputStream {
 
-    private static final String LOG = "MMSInputStream"; 
+	private static final String CHARSET_NAME_UNICODE = "UTF-16LE";
 
-    private static boolean libLoaded = false;
+	private static boolean libLoaded = false;
 
+	// //////////////////////////////////////////////////////////////////////////
+	// Attributes
+	// //////////////////////////////////////////////////////////////////////////
 
-    ////////////////////////////////////////////////////////////////////////////
-    // Attributes
-    ////////////////////////////////////////////////////////////////////////////
+	private static final String LOG = "MMSInputStream";
 
-    /**
-     * The native handler.
-     */
-    private int mms;
+	/**
+	 * This is a conversion method used by libmms.
+	 */
+	private static synchronized void ensureLibLoaded() {
+		if( !MMSInputStream.libLoaded ) {
+			System.loadLibrary( "mms" );
+			MMSInputStream.libLoaded = true;
+		}
+	}
 
-    /**
-     * 
-     */
-    private int len;
+	// //////////////////////////////////////////////////////////////////////////
+	// Constructors
+	// //////////////////////////////////////////////////////////////////////////
 
+	/**
+	 * This is a conversion method used by libmms.
+	 */
+	private static byte[] string2unicode( final String s ) {
+		Log.d( MMSInputStream.LOG, "string2unicode(): '" + s + "'" );
 
-    ////////////////////////////////////////////////////////////////////////////
-    // Constructors
-    ////////////////////////////////////////////////////////////////////////////
+		// return s.getBytes( CHARSET_UNICODE );
 
-    public MMSInputStream( String url ) throws IOException {
-        ensureLibLoaded();
+		try {
+			return s.getBytes( MMSInputStream.CHARSET_NAME_UNICODE );
+		} catch( final java.io.UnsupportedEncodingException e ) {
+			Log.e( MMSInputStream.LOG, "Cannot convert string --> unicode", e );
+			throw new RuntimeException( e );
+		}
+	}
 
-        this.mms = nativeConnect( url );
-        this.len = nativeGetLength( this.mms );
-    }
+	// //////////////////////////////////////////////////////////////////////////
+	// Public - InputStream
+	// //////////////////////////////////////////////////////////////////////////
 
+	/**
+	 * This is a conversion method used by libmms.
+	 */
+	private static String unicode2string( final byte[] bytes ) {
+		try {
+			final String ret = new String( bytes, MMSInputStream.CHARSET_NAME_UNICODE );
+			Log.d( MMSInputStream.LOG, "unicode2string(): '" + ret + "'" );
 
-    ////////////////////////////////////////////////////////////////////////////
-    // Public - InputStream
-    ////////////////////////////////////////////////////////////////////////////
-    
-    public int length() {
-    	return this.len;
-    }
+			return ret;
+		} catch( final java.io.UnsupportedEncodingException e ) {
+			Log.e( MMSInputStream.LOG, "Cannot convert unicode --> string", e );
+			throw new RuntimeException( e );
+		}
+	}
 
-    /**
-     * Please do not use this method - this is not efficient !
-     */
-    public int read() throws IOException {
-        byte[] buf = new byte[1];
+	/**
+	 * 
+	 */
+	private final int len;
 
-        int n;
-        while ((n = read( buf, 0, 1)) == 0) {
-        }
+	/**
+	 * The native handler.
+	 */
+	private final int mms;
 
-        if (n == 1) return ((int)buf[0]) & 0xff;
-        return n;
-    }
+	public MMSInputStream( final String url ) throws IOException {
+		MMSInputStream.ensureLibLoaded();
 
+		this.mms = this.nativeConnect( url );
+		this.len = this.nativeGetLength( this.mms );
+	}
 
-    @Override
-    public int read(byte[] b, int off, int len) throws IOException {
-        return nativeRead( mms, b, off, len );
-    }
+	// //////////////////////////////////////////////////////////////////////////
+	// Private
+	// //////////////////////////////////////////////////////////////////////////
 
+	@Override
+	public void close() throws IOException {
+		this.nativeClose( this.mms );
+	}
 
-    @Override
-    public void close() throws IOException {
-        nativeClose( mms );
-    }
+	public int length() {
+		return this.len;
+	}
 
+	private native void nativeClose( int mms ) throws IOException;
 
-    ////////////////////////////////////////////////////////////////////////////
-    // Private
-    ////////////////////////////////////////////////////////////////////////////
+	private native int nativeConnect( String url ) throws IOException;
 
-    /**
-     * This is a conversion method used by libmms.
-     */
-    private static synchronized void ensureLibLoaded() {
-        if (!libLoaded) {
-            System.loadLibrary( "mms" );
-            libLoaded = true;
-        }
-    }
+	private native int nativeGetLength( int mms ) throws IOException;
 
+	private native int nativeRead( int mms, byte[] b, int off, int len ) throws IOException;
 
-    private static final String CHARSET_NAME_UNICODE = "UTF-16LE";
+	/**
+	 * Please do not use this method - this is not efficient !
+	 */
+	@Override
+	public int read() throws IOException {
+		final byte[] buf = new byte[ 1 ];
 
-    /**
-     * This is a conversion method used by libmms.
-     */
-    private static byte[] string2unicode( String s ) {
-        Log.d( LOG, "string2unicode(): '" + s  + "'" );
+		int n;
+		while( (n = this.read( buf, 0, 1 )) == 0 ) {
+		}
 
-        // return s.getBytes( CHARSET_UNICODE );
+		if( n == 1 ) {
+			return (buf[ 0 ]) & 0xff;
+		}
+		return n;
+	}
 
-        try {
-            return s.getBytes( CHARSET_NAME_UNICODE );
-        }
-        catch (java.io.UnsupportedEncodingException e) {
-            Log.e( LOG, "Cannot convert string --> unicode", e );
-            throw new RuntimeException( e );
-        }
-    }
-
-    /**
-     * This is a conversion method used by libmms.
-     */
-    private static String unicode2string( byte[] bytes ) {
-        try {
-            String ret = new String( bytes, CHARSET_NAME_UNICODE );
-            Log.d( LOG, "unicode2string(): '" + ret  + "'" );
-
-            return ret;
-        }
-        catch (java.io.UnsupportedEncodingException e) {
-            Log.e( LOG, "Cannot convert unicode --> string", e );
-            throw new RuntimeException( e );
-        }
-    }
-
-
-    private native int nativeConnect( String url ) throws IOException;
-    private native int nativeRead( int mms, byte[] b, int off, int len ) throws IOException;
-    private native void nativeClose( int mms ) throws IOException;
-    private native int nativeGetLength( int mms ) throws IOException;
+	@Override
+	public int read( final byte[] b, final int off, final int len ) throws IOException {
+		return this.nativeRead( this.mms, b, off, len );
+	}
 
 }
