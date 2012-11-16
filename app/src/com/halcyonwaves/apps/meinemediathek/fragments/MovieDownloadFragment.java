048aa90f5ee8047d79a66d1c993e9b7fe810e754
Tim Hütz <tim@huetz.biz>
2012-11-15 21:57:43 -0500

    Set up a mock UI for demonstrating the idea

--- app/src/com/halcyonwaves/apps/meinemediathek/fragments/MovieDownloadFragment.java
+++ app/src/com/halcyonwaves/apps/meinemediathek/fragments/MovieDownloadFragment.java
@@ -4,9 +4,7 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -15,28 +13,26 @@ import com.halcyonwaves.apps.meinemediathek.worker.DownloadStreamTask;
 
 public class MovieDownloadFragment extends Fragment {
 
-	private Button btn = null;
 	private ProgressBar pb = null;
 	private TextView tv = null;
 
 	@Override
 	public View onCreateView( final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState ) {
 		final View v = inflater.inflate( R.layout.fragment_moviedownload, container );
-		this.btn = (Button) v.findViewById( R.id.btn_download );
 		this.pb = (ProgressBar) v.findViewById( R.id.pb_download_progress_pct );
 		this.tv = (TextView) v.findViewById( R.id.tv_download_progress_bytes );
 
-		this.btn.setOnClickListener( new OnClickListener() {
-
-			@Override
-			public void onClick( final View v ) {
-
-				final DownloadStreamTask dst = new DownloadStreamTask( MovieDownloadFragment.this.pb, MovieDownloadFragment.this.tv );
-				dst.execute( "mms://a1014.v1252931.c125293.g.vm.akamaistream.net/7/1014/125293/v0001/wm.od.origin.zdf.de.gl-systemhaus.de/none/zdf/12/08/120827_trailer_staffel13_kio_sok4_vh.wmv" );
-
-			}
-		} );
-
 		return v;
+	}
+
+	@Override
+	public void onViewCreated( View view, Bundle savedInstanceState ) {
+		//
+		super.onViewCreated( view, savedInstanceState );
+
+		//
+		final DownloadStreamTask dst = new DownloadStreamTask( MovieDownloadFragment.this.pb, MovieDownloadFragment.this.tv );
+		dst.execute( "mms://a1014.v1252931.c125293.g.vm.akamaistream.net/7/1014/125293/v0001/wm.od.origin.zdf.de.gl-systemhaus.de/none/zdf/12/08/120827_trailer_staffel13_kio_sok4_vh.wmv" );
+
 	}
 }
