4d59221312d61694cf3584c31f3587b92d8690d5
Tim Hütz <tim@huetz.biz>
2012-11-15 21:03:07 -0500

    Changed the layout of the download fragment

--- app/src/com/halcyonwaves/apps/meinemediathek/fragments/MovieDownloadFragment.java
+++ app/src/com/halcyonwaves/apps/meinemediathek/fragments/MovieDownloadFragment.java
@@ -16,7 +16,7 @@ import android.widget.Button;
 import com.halcyonwaves.apps.meinemediathek.R;
 import com.halcyonwaves.apps.meinemediathek.ndk.MMSInputStream;
 
-public class MovieSearchFragment extends Fragment {
+public class MovieDownloadFragment extends Fragment {
 
 	private Button btn = null;
 
@@ -30,15 +30,15 @@ public class MovieSearchFragment extends Fragment {
 	
 	@Override
 	public View onCreateView( final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState ) {
-		View v = inflater.inflate( R.layout.fragment_moviesearch, container );
-		this.btn = (Button) v.findViewById( R.id.button1 );
+		View v = inflater.inflate( R.layout.fragment_moviedownload, container );
+		this.btn = (Button) v.findViewById( R.id.btn_download );
 		this.btn.setOnClickListener( new OnClickListener() {
 
 			@Override
 			public void onClick( View v ) {
 				try {
 					MMSInputStream st = new MMSInputStream( "mms://a1014.v1252931.c125293.g.vm.akamaistream.net/7/1014/125293/v0001/wm.od.origin.zdf.de.gl-systemhaus.de/none/zdf/12/08/120827_trailer_staffel13_kio_sok4_vh.wmv" );
-					MovieSearchFragment.this.createOutput();
+					MovieDownloadFragment.this.createOutput();
 					byte[] buffer = new byte[ 8192 ];
 					int comReadB  = 0;
 					while( comReadB < st.length() ) {
