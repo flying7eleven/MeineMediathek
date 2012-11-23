8f9cdacc351a69d489b94c3faebd9812692cc593
Tim Hütz <tim@huetz.biz>
2012-11-22 20:31:36 -0500

    Added the missing source files of the used preference constants

---
+++ app/src/com/halcyonwaves/apps/meinemediathek/Consts.java
@@ -0,0 +1,14 @@
+package com.halcyonwaves.apps.meinemediathek;
+
+public class Consts {
+
+	/**
+	 * This preference key is used to store if the user accepted the license of this application.
+	 */
+	public final static String PREFERENCE_LICENSE_ACCEPTED = "license.accepted";
+
+	/**
+	 * This preference key stores the time when the user accepted the license (for legal purposes).
+	 */
+	public final static String PREFERENCE_LICENSE_AGREEMENT_TIME = "license.accepted.time";
+}
