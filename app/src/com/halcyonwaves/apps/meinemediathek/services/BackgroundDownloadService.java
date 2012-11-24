e1a2dd162b5c35b7d443a3ed0c2c3bfd819909b9
Tim Hütz <tim@huetz.biz>
2012-11-24 18:57:47 -0500

    Fixed a bug which caused an crash of the BackgroundDownloadingService

--- app/src/com/halcyonwaves/apps/meinemediathek/services/BackgroundDownloadService.java
+++ app/src/com/halcyonwaves/apps/meinemediathek/services/BackgroundDownloadService.java
@@ -2,6 +2,7 @@ package com.halcyonwaves.apps.meinemediathek.services;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -18,18 +19,24 @@ public class BackgroundDownloadService extends Service {
 
 	@Override
 	public int onStartCommand( final Intent intent, final int flags, final int startid ) {
-		// get some required information to starting the download
-		final String episodeTitle = intent.getExtras().getString( "movieTitle" );
-		final String downlaodURL = intent.getExtras().getString( "downloadLink" );
+		// get the extras into a local variable
+		final Bundle suppliedExtras = intent.getExtras();
 
-		// just log the event that we received a download request
-		Log.d( BackgroundDownloadService.TAG, "Service started with request to download following URL: " + downlaodURL );
+		if( null != suppliedExtras ) {
+			// get some required information to starting the download
+			final String episodeTitle = suppliedExtras.getString( "movieTitle" );
+			final String downlaodURL = suppliedExtras.getString( "downloadLink" );
 
-		// we have to check if the download was already started and skip the rest if it was
-		// TODO: this
+			// just log the event that we received a download request
+			Log.d( BackgroundDownloadService.TAG, "Service started with request to download following URL: " + downlaodURL );
 
-		// do the actual download in a separate thread
-		new DownloadStreamThread( this.getApplicationContext(), downlaodURL, episodeTitle ).start();
+			// we have to check if the download was already started and skip the rest if it was
+			// TODO: this
+
+			// do the actual download in a separate thread
+			new DownloadStreamThread( this.getApplicationContext(), downlaodURL, episodeTitle ).start();
+
+		}
 
 		//
 		return Service.START_STICKY;
