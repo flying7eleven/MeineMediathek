0deff308dd3702146fd78b8517eb45ccd2f4f6fd
Tim Hütz <tim@huetz.biz>
2012-12-02 19:52:29 -0500

    The cancel download request dialog can now connect to the background service

--- app/src/com/halcyonwaves/apps/meinemediathek/activities/ManageDownloadActivity.java
+++ app/src/com/halcyonwaves/apps/meinemediathek/activities/ManageDownloadActivity.java
@@ -1,6 +1,13 @@
 package com.halcyonwaves.apps.meinemediathek.activities;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Messenger;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -8,6 +15,7 @@ import android.widget.TextView;
 
 import com.halcyonwaves.apps.meinemediathek.Consts;
 import com.halcyonwaves.apps.meinemediathek.R;
+import com.halcyonwaves.apps.meinemediathek.services.BackgroundDownloadService;
 
 public class ManageDownloadActivity extends BaseActivity {
 
@@ -15,9 +23,47 @@ public class ManageDownloadActivity extends BaseActivity {
 	private TextView movieTitle = null;
 	private TextView movieDescription = null;
 
+	private static final String TAG = "ManageDownloadActivity";
+	
+	private Messenger serviceMessanger = null;
+	private final ServiceConnection serviceConnection = new ServiceConnection() {
+
+		public void onServiceConnected( final ComponentName className, final IBinder service ) {
+			ManageDownloadActivity.this.serviceMessanger = new Messenger( service );
+			Log.d( ManageDownloadActivity.TAG, "Conntected to download service" );
+		}
+
+		public void onServiceDisconnected( final ComponentName className ) {
+			ManageDownloadActivity.this.serviceMessanger = null;
+			Log.d( ManageDownloadActivity.TAG, "Disconnected from download service" );
+		}
+	};
+
+	private void doBindService() {
+		if( null == this.serviceMessanger ) {
+			this.bindService( new Intent( this, BackgroundDownloadService.class ), this.serviceConnection, Context.BIND_AUTO_CREATE );
+		}
+	}
+
+	private void doUnbindService() {
+		if( null != this.serviceMessanger ) {
+			this.unbindService( this.serviceConnection );
+			this.serviceMessanger = null; // we have to do this because the onServiceDisconnected method gets just called if the service was killed
+		}
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		this.doUnbindService();
+	}
+
 	@Override
 	protected void onCreate( final Bundle savedInstanceState ) {
 		super.onCreate( savedInstanceState );
+		
+		// bind to the download service
+		this.doBindService();
 
 		// set the layout of the activity (which is use as a dialog)
 		this.setContentView( R.layout.activity_managedownload );
